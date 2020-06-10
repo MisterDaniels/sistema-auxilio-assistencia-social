d34a9d41445b6b3629236786c1656afd13506d6e
Daniel Silva <dodosilva51@gmail.com>
2020-06-10 00:25:26 -0300

    Ajustado MDCDataTable

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.set('view engine', 'njk');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 nunjucks.configure(path.join(__dirname, 'views'), {
     autoescape: true,
     express: app
